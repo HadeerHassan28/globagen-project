a65fcc6ebcc03ad6cc6c275d35f754e9e5850e4a
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-07-12 22:55:56 +0300

    contact is created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
 import Layout from "./components/Layout/Layout";
@@ -26,6 +27,10 @@ function App() {
         {
           path: "research",
           element: <Research />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
         },
       ],
     },
