8429b1c37d884d752a73de936805c06d94807c68
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-07-12 14:01:13 +0300

    try to do navbar

---
+++ src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./Home.module.css";
+import { useTranslation } from "react-i18next";
+const Home = () => {
+  const { t } = useTranslation();
+  return <div className={styles.Home}>{t("Home")}</div>;
+};
+
+export default Home;
