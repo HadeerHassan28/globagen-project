710178bf47b5ff5af04f69409266793ce63b9e24
ahmed-farrag <ahmedfaarrag@gmail.com>
2023-07-12 14:40:52 +0300

    solved navbar issue

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,50 +1,34 @@
-import * as React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-
-const NavBar = () => {
-  const { t } = useTranslation();
+function BasicExample() {
   return (
-    <>
-      <nav className="navbar navbar-light bg-light">
-        <Link className="navbar-brand" href="Nav">
-          <img
-            src="/docs/4.0/assets/brand/bootstrap-solid.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="logo"
-          />
-          Bootstrap
-        </Link>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link className="nav-link" href="nav">
-                Home <span className="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="nav">
-                Features
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="nav">
-                Pricing
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link disabled" href="nav">
-                Disabled
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
-export default NavBar;
+export default BasicExample;
