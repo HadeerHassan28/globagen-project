8f7fd2075f660393120a73852fac80ef6b4c711f
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-07-14 17:14:56 +0300

    contact without desgin

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,28 +2,48 @@ import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { conTheme } from "../../Context/Context";
 import NearMeOutlinedIcon from "@mui/icons-material/NearMeOutlined";
-import { BsTelephoneOutbound } from "react-icons/bs";
+import { BsTelephoneOutbound, BsEnvelope } from "react-icons/bs";
 import { AiOutlineWhatsApp } from "react-icons/ai";
 import { Link } from "react-router-dom";
+
 const Contact = () => {
   const { t } = useTranslation();
   let { isTheme } = useContext(conTheme);
+  // const address =
+  //   "64-62 Mecca, Sheraton Al Matar, El Nozha, Cairo Governorate 4471206";
+
   const contactInfo = [
     {
       id: 1,
       title: "Shipping & Customs Clearance Department",
-      name: "Mr. Mohamed Fathy",
+      name: t("Mr. Mohamed Fathy"),
       number: "01001539990",
       email: "m.fathy@globagene.com",
     },
     {
       id: 2,
       title: "Scientific Research Department",
-      name: "Eng. Nora Ragheb",
+      name: t("Eng. Nora Ragheb"),
       number: "01067652075",
       email: "nora.ragheb@globagene.com",
     },
+    {
+      id: 3,
+      title: "General Requirement",
+      number: "01033569793",
+      email: "info@globagene.com",
+    },
   ];
+  const handleEmailClick = (email) => {
+    window.location.href = `mailto:${email}`;
+  };
+  // const getMapImageSrc = (address) => {
+  //   const apiKey = "AIzaSyA67i2eU-V4PS5QdKS-wOaLB7WyMmAR7ZQ";
+  //   const encodedAddress = encodeURIComponent(address);
+  //   const imageUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${encodedAddress}&zoom=14&size=600x300&key=${apiKey}`;
+  //   return imageUrl;
+  // };
+  // const mapImageSrc = getMapImageSrc(address);
   return (
     <div
       className={`container`}
@@ -42,40 +62,75 @@ const Contact = () => {
         {t("Contact us")}
       </h1>
       {contactInfo.map((ele, index) => (
-        <div
-          key={ele.id}
-          className={`container d-flex justify-content-sm-center align-items-sm-center `}
-        >
-          <p style={{ color: isTheme === true ? "white" : "#071848" }}>
+        <div key={ele.id} className={`container `}>
+          <p
+            className={`d-flex justify-content-sm-center align-items-sm-center flex-column h5`}
+            style={{ color: isTheme === true ? "white" : "#071848" }}
+          >
             {ele.title}
           </p>
-          <p style={{ color: isTheme === true ? "white" : "#071848" }}>
+          <p
+            className={`d-flex justify-content-sm-center align-items-sm-center flex-column h6`}
+            style={{ color: isTheme === true ? "white" : "#071848" }}
+          >
             {ele.name}
           </p>
-          <Link to={`tel:+${ele.number}`}>
-            <BsTelephoneOutbound
-              className="me-2"
-              target="_blank"
-              style={{ color: isTheme === true ? "white" : "#071848" }}
+          <div className=" d-flex justify-content-sm-center align-items-sm-center flex-column ">
+            <Link to={`tel:+${ele.number}`}>
+              <BsTelephoneOutbound
+                target="_blank"
+                style={{ color: isTheme === true ? "white" : "#071848" }}
+              />
+              <span
+                style={{
+                  marginLeft: "0.5rem",
+                  color: isTheme ? "black" : "#071848",
+                }}
+              >
+                {ele.number}
+              </span>
+            </Link>
+          </div>
+          <div className=" d-flex justify-content-sm-center align-items-sm-center flex-column mt-2">
+            <Link to={`https://wa.me/+${ele.number}`}>
+              <AiOutlineWhatsApp
+                target="_blank"
+                style={{ color: isTheme === true ? "white" : "#071848" }}
+              />
+              <span
+                style={{
+                  marginLeft: "0.5rem",
+                  color: isTheme ? "black" : "#071848",
+                }}
+              >
+                {ele.number}
+              </span>
+            </Link>
+            <div
+              className=" d-flex align-items-center   mt-2 mb-3"
+              onClick={() => handleEmailClick(ele.email)}
+              style={{ cursor: "pointer" }}
             >
-              {ele.number}
-            </BsTelephoneOutbound>
-          </Link>
-          <Link to={`https://wa.me/+${ele.number}`}>
-            <AiOutlineWhatsApp
-              className="me-2"
-              target="_blank"
-              style={{ color: isTheme === true ? "white" : "#071848" }}
-            >
-              {ele.number}
-            </AiOutlineWhatsApp>
-          </Link>
+              <BsEnvelope
+                target="_blank"
+                style={{ color: isTheme === true ? "white" : "#071848" }}
+              />
+              <span
+                style={{
+                  marginLeft: "0.5rem",
+                  color: isTheme ? "black" : "#071848",
+                }}
+              >
+                {ele.email}
+              </span>
+            </div>
+          </div>
           {index !== contactInfo.length - 1 && (
             <hr
               key={`hr-${ele.id}`}
               style={{
                 backgroundColor: isTheme === true ? "#0FEF96" : "#071848",
-                height: 10,
+                height: 3,
                 width: "100%",
                 margin: "1rem 0",
               }}
@@ -83,6 +138,9 @@ const Contact = () => {
           )}
         </div>
       ))}
+      <div className="container d-flex justify-content-center align-items-center">
+        {/* <img src={} alt="Google Map" /> */}
+      </div>
     </div>
   );
 };
